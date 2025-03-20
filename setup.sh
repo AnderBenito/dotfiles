8904a37f7bcce7d22b63874a4eeed6b68f01b6e6
abenito <anderbenito96@gmail.com>
2025-03-20 19:03:26 +0100

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # install.sh
 # symlink you dotfiles to appropriate places
 
@@ -11,5 +12,11 @@ DOTFILES=(
 
 for dotfile in "${DOTFILES[@]}";do
 	rm -rf "${HOME}/${dotfile}"
+
+	TARGET_DIR="${HOME}/$(dirname "${dotfile}")"
+
+	# Ensure the directory exists
+	mkdir -p "${TARGET_DIR}"
+
 	ln -sf "${DIR}/${dotfile}" "${HOME}/${dotfile}"
 done
